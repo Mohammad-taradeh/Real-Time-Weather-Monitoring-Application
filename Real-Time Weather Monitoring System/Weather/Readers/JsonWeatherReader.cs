3ca595cafacee14d4b589b23cd902bfecf222b14
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-20 19:28:51 +0300

    create the logic needed to parse weather data to Json/XML

---
+++ Real-Time Weather Monitoring System/Weather/Readers/JsonWeatherReader.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System.Text.Json;
+using Real_Time_Weather_Monitoring_System.Weather.models;
+
+namespace Real_Time_Weather_Monitoring_System.Weather.Readers;
+
+internal class JsonWeatherReader : IWeatherReader
+{
+    public IWeatherData? ReadWeather(StringBuilder sb)
+    {
+        return JsonSerializer.Deserialize<WeatherData>(sb.ToString());
+    }
+}
---
+++ Real-Time Weather Monitoring System/Weather/Readers/XmlWeatherReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+using Real_Time_Weather_Monitoring_System.Weather.models;
+
+namespace Real_Time_Weather_Monitoring_System.Weather.Readers;
+
+internal class XmlWeatherReader : IWeatherReader
+{
+    public IWeatherData? ReadWeather(StringBuilder sb)
+    {
+        var xmlSerializer = new XmlSerializer(typeof(WeatherData));
+        WeatherData? result;
+        Console.WriteLine(sb.ToString());
+        return (WeatherData)xmlSerializer.Deserialize(new StringReader(sb.ToString()));
+    }
+}
