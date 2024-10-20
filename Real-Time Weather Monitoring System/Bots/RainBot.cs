5ce45fc4564f518512c5e59601d8becf73042e07
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-20 19:23:12 +0300

    create the bots needed

---
+++ Real-Time Weather Monitoring System/Bots/RainBot.cs
@@ -0,0 +1,18 @@
+﻿using Real_Time_Weather_Monitoring_System.Weather.models;
+
+namespace Real_Time_Weather_Monitoring_System.Bots;
+
+public class RainBot : IBaseBot
+{
+    public bool Enabled { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public double HumidityThreshold { get; set; }
+    public void Activate(IWeatherData data)
+    {
+        if(Enabled && data.Humidity > HumidityThreshold)
+        {
+            Console.WriteLine("RainBot Activated!");
+            Console.WriteLine($"RainBot: {Message}");
+        }
+    }
+}
---
+++ Real-Time Weather Monitoring System/Bots/SnowBot.cs
@@ -0,0 +1,19 @@
+﻿using Real_Time_Weather_Monitoring_System.Weather.models;
+
+namespace Real_Time_Weather_Monitoring_System.Bots;
+
+public class SnowBot : IBaseBot
+{
+    public bool Enabled { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public double TemperatureThreshold { get; set; }
+
+    public void Activate(IWeatherData data)
+    {
+        if (Enabled && data.Temperature < TemperatureThreshold)
+        {
+            Console.WriteLine("SnowBot Activated!");
+            Console.WriteLine($"SnowBot: {Message}");
+        }
+    }
+}
