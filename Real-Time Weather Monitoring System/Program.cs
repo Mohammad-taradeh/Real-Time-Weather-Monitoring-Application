775671a74ee06664896e845bf59018dbe7efee76
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-20 19:33:20 +0300

    configure the app in the main

---
+++ Real-Time Weather Monitoring System/Program.cs
@@ -0,0 +1,29 @@
+﻿using Real_Time_Weather_Monitoring_System.Config;
+using Real_Time_Weather_Monitoring_System.Station;
+using Real_Time_Weather_Monitoring_System.Weather.models;
+using Real_Time_Weather_Monitoring_System.Weather.Readers.Factory;
+using System.Text;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+
+        WeatherStation station = new(SystemBots.Instance);
+        Console.WriteLine("Enter weather data:");
+        StringBuilder sb = new StringBuilder();
+        string? input;
+        while (true)
+        {
+            input = Console.ReadLine();
+            if ( input != "STOP")
+                sb.Append(input);
+            else break;
+        }
+
+        WeatherReaderFactory readerFactory = new();
+        IWeatherData weatherData = readerFactory.GetWeatherData(sb);
+        station.Notify(weatherData);
+
+    }
+}
