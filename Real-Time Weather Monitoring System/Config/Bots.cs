27b6457d7d87ad9eae089b2afc19cab3ca0b555d
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-20 19:31:11 +0300

    create the config reader

---
+++ Real-Time Weather Monitoring System/Config/Bots.cs
@@ -0,0 +1,10 @@
+﻿using Real_Time_Weather_Monitoring_System.Bots;
+
+namespace Real_Time_Weather_Monitoring_System.Config;
+
+public class Bots : IBots
+{
+    public RainBot? rainBot { get; set; }
+    public SnowBot? snowBot { get; set; }
+    public SunBot? sunBot { get; set; }
+}
