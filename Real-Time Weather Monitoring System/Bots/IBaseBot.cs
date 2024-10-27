b739e045982f45aa431cc719ed9d6eb2fd7a7a43
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-27 10:55:29 +0200

    update the interface and it's dependancies

--- Real-Time Weather Monitoring System/Bots/IBaseBot.cs
+++ Real-Time Weather Monitoring System/Bots/IBaseBot.cs
@@ -5,6 +5,6 @@ namespace RealTimeWeatherMonitoringSystem.Bots;
 public interface IBaseBot
 {
     public string Message { get; set; }
-    public bool Enabled { get; set; }
-    public abstract void Activate(IWeatherData data);
+    public bool IsEnabled { get; set; }
+    public void Activate(IWeatherData data);
 }
--- Real-Time Weather Monitoring System/Bots/SnowBot.cs
+++ Real-Time Weather Monitoring System/Bots/SnowBot.cs
@@ -3,13 +3,13 @@
 namespace RealTimeWeatherMonitoringSystem.Bots;
 public class SnowBot : IBaseBot
 {
-    public bool Enabled { get; set; }
+    public bool IsEnabled { get; set; }
     public string Message { get; set; } = string.Empty;
     public double TemperatureThreshold { get; set; }
 
     public void Activate(IWeatherData data)
     {
-        if (Enabled && data.Temperature < TemperatureThreshold)
+        if (IsEnabled && data.Temperature < TemperatureThreshold)
         {
             Console.WriteLine("SnowBot Activated!");
             Console.WriteLine($"SnowBot: {Message}");
